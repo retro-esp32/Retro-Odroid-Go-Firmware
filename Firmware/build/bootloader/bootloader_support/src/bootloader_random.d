5e53c4c00effa424b4ad21f246350b57ee58d264
32teeth <eugene.andruszczenko@gmail.com>
2020-02-10 16:09:48 -0500

    Mon 10 Feb 2020 16:09:48 EST - fixed paths

--- Firmware/build/bootloader/bootloader_support/src/bootloader_random.d
+++ Firmware/build/bootloader/bootloader_support/src/bootloader_random.d
@@ -1,93 +1,93 @@
 src/bootloader_random.o: \
- /Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/bootloader_support/src/bootloader_random.c \
- /Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/bootloader_support/include/bootloader_random.h \
- /Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/soc/esp32/include/soc/cpu.h \
- /Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/esp32/include/xtensa/corebits.h \
- /Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/esp32/include/xtensa/config/core.h \
- /Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/esp32/include/xtensa/hal.h \
- /Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/esp32/include/xtensa/xtensa-versions.h \
- /Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/esp32/include/xtensa/config/core-isa.h \
- /Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/esp32/include/xtensa/config/core-matmap.h \
- /Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/esp32/include/xtensa/config/tie.h \
- /Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/soc/esp32/include/soc/wdev_reg.h \
- /Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/soc/esp32/include/soc/soc.h \
- /Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/esp32/include/esp_assert.h \
- /Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/soc/esp32/include/soc/rtc_cntl_reg.h \
- /Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/soc/esp32/include/soc/sens_reg.h \
- /Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/soc/esp32/include/soc/syscon_reg.h \
- /Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/soc/esp32/include/soc/dport_reg.h \
- /Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/soc/esp32/include/soc/dport_access.h \
- /Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/esp32/include/esp_attr.h \
- /Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/esp32/include/esp_dport_access.h \
- /Users/eugene/Desktop/github/RetroESP32/Public/Firmware/Firmware/build/bootloader/../include/sdkconfig.h \
- /Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/soc/esp32/include/soc/uart_reg.h \
- /Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/esp32/include/xtensa/xtruntime.h \
- /Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/esp32/include/xtensa/config/specreg.h \
- /Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/esp32/include/xtensa/xtruntime-core-state.h \
- /Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/esp32/include/xtensa/xtruntime-frames.h \
- /Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/soc/esp32/include/soc/i2s_reg.h \
- /Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/log/include/esp_log.h \
- /Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/esp32/include/rom/ets_sys.h \
- /Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/soc/esp32/include/soc/soc.h \
- /Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/log/include/esp_log_internal.h
+ /Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/bootloader_support/src/bootloader_random.c \
+ /Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/bootloader_support/include/bootloader_random.h \
+ /Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/soc/esp32/include/soc/cpu.h \
+ /Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/esp32/include/xtensa/corebits.h \
+ /Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/esp32/include/xtensa/config/core.h \
+ /Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/esp32/include/xtensa/hal.h \
+ /Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/esp32/include/xtensa/xtensa-versions.h \
+ /Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/esp32/include/xtensa/config/core-isa.h \
+ /Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/esp32/include/xtensa/config/core-matmap.h \
+ /Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/esp32/include/xtensa/config/tie.h \
+ /Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/soc/esp32/include/soc/wdev_reg.h \
+ /Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/soc/esp32/include/soc/soc.h \
+ /Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/esp32/include/esp_assert.h \
+ /Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/soc/esp32/include/soc/rtc_cntl_reg.h \
+ /Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/soc/esp32/include/soc/sens_reg.h \
+ /Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/soc/esp32/include/soc/syscon_reg.h \
+ /Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/soc/esp32/include/soc/dport_reg.h \
+ /Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/soc/esp32/include/soc/dport_access.h \
+ /Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/esp32/include/esp_attr.h \
+ /Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/esp32/include/esp_dport_access.h \
+ /Users/andrueu/Desktop/github/R32/Odroid/Firmware/build/bootloader/../include/sdkconfig.h \
+ /Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/soc/esp32/include/soc/uart_reg.h \
+ /Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/esp32/include/xtensa/xtruntime.h \
+ /Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/esp32/include/xtensa/config/specreg.h \
+ /Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/esp32/include/xtensa/xtruntime-core-state.h \
+ /Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/esp32/include/xtensa/xtruntime-frames.h \
+ /Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/soc/esp32/include/soc/i2s_reg.h \
+ /Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/log/include/esp_log.h \
+ /Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/esp32/include/rom/ets_sys.h \
+ /Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/soc/esp32/include/soc/soc.h \
+ /Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/log/include/esp_log_internal.h
 
-/Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/bootloader_support/include/bootloader_random.h:
+/Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/bootloader_support/include/bootloader_random.h:
 
-/Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/soc/esp32/include/soc/cpu.h:
+/Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/soc/esp32/include/soc/cpu.h:
 
-/Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/esp32/include/xtensa/corebits.h:
+/Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/esp32/include/xtensa/corebits.h:
 
-/Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/esp32/include/xtensa/config/core.h:
+/Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/esp32/include/xtensa/config/core.h:
 
-/Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/esp32/include/xtensa/hal.h:
+/Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/esp32/include/xtensa/hal.h:
 
-/Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/esp32/include/xtensa/xtensa-versions.h:
+/Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/esp32/include/xtensa/xtensa-versions.h:
 
-/Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/esp32/include/xtensa/config/core-isa.h:
+/Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/esp32/include/xtensa/config/core-isa.h:
 
-/Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/esp32/include/xtensa/config/core-matmap.h:
+/Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/esp32/include/xtensa/config/core-matmap.h:
 
-/Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/esp32/include/xtensa/config/tie.h:
+/Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/esp32/include/xtensa/config/tie.h:
 
-/Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/soc/esp32/include/soc/wdev_reg.h:
+/Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/soc/esp32/include/soc/wdev_reg.h:
 
-/Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/soc/esp32/include/soc/soc.h:
+/Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/soc/esp32/include/soc/soc.h:
 
-/Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/esp32/include/esp_assert.h:
+/Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/esp32/include/esp_assert.h:
 
-/Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/soc/esp32/include/soc/rtc_cntl_reg.h:
+/Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/soc/esp32/include/soc/rtc_cntl_reg.h:
 
-/Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/soc/esp32/include/soc/sens_reg.h:
+/Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/soc/esp32/include/soc/sens_reg.h:
 
-/Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/soc/esp32/include/soc/syscon_reg.h:
+/Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/soc/esp32/include/soc/syscon_reg.h:
 
-/Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/soc/esp32/include/soc/dport_reg.h:
+/Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/soc/esp32/include/soc/dport_reg.h:
 
-/Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/soc/esp32/include/soc/dport_access.h:
+/Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/soc/esp32/include/soc/dport_access.h:
 
-/Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/esp32/include/esp_attr.h:
+/Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/esp32/include/esp_attr.h:
 
-/Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/esp32/include/esp_dport_access.h:
+/Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/esp32/include/esp_dport_access.h:
 
-/Users/eugene/Desktop/github/RetroESP32/Public/Firmware/Firmware/build/bootloader/../include/sdkconfig.h:
+/Users/andrueu/Desktop/github/R32/Odroid/Firmware/build/bootloader/../include/sdkconfig.h:
 
-/Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/soc/esp32/include/soc/uart_reg.h:
+/Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/soc/esp32/include/soc/uart_reg.h:
 
-/Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/esp32/include/xtensa/xtruntime.h:
+/Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/esp32/include/xtensa/xtruntime.h:
 
-/Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/esp32/include/xtensa/config/specreg.h:
+/Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/esp32/include/xtensa/config/specreg.h:
 
-/Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/esp32/include/xtensa/xtruntime-core-state.h:
+/Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/esp32/include/xtensa/xtruntime-core-state.h:
 
-/Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/esp32/include/xtensa/xtruntime-frames.h:
+/Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/esp32/include/xtensa/xtruntime-frames.h:
 
-/Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/soc/esp32/include/soc/i2s_reg.h:
+/Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/soc/esp32/include/soc/i2s_reg.h:
 
-/Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/log/include/esp_log.h:
+/Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/log/include/esp_log.h:
 
-/Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/esp32/include/rom/ets_sys.h:
+/Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/esp32/include/rom/ets_sys.h:
 
-/Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/soc/esp32/include/soc/soc.h:
+/Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/soc/esp32/include/soc/soc.h:
 
-/Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/log/include/esp_log_internal.h:
-/Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/bootloader_support/src/bootloader_random.c:
+/Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/log/include/esp_log_internal.h:
+/Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/bootloader_support/src/bootloader_random.c:
--- Firmware/build/bootloader/bootloader_support/src/efuse.d
+++ Firmware/build/bootloader/bootloader_support/src/efuse.d
@@ -1,36 +1,36 @@
 src/efuse.o: \
- /Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/bootloader_support/src/efuse.c \
- /Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/bootloader_support/include/esp_efuse.h \
- /Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/soc/esp32/include/soc/efuse_reg.h \
- /Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/soc/esp32/include/soc/soc.h \
- /Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/esp32/include/esp_assert.h \
- /Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/esp32/include/esp_err.h \
- /Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/log/include/esp_log.h \
- /Users/eugene/Desktop/github/RetroESP32/Public/Firmware/Firmware/build/bootloader/../include/sdkconfig.h \
- /Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/esp32/include/rom/ets_sys.h \
- /Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/soc/esp32/include/soc/soc.h \
- /Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/log/include/esp_log_internal.h \
- /Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/bootloader_support/include/bootloader_random.h
+ /Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/bootloader_support/src/efuse.c \
+ /Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/bootloader_support/include/esp_efuse.h \
+ /Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/soc/esp32/include/soc/efuse_reg.h \
+ /Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/soc/esp32/include/soc/soc.h \
+ /Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/esp32/include/esp_assert.h \
+ /Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/esp32/include/esp_err.h \
+ /Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/log/include/esp_log.h \
+ /Users/andrueu/Desktop/github/R32/Odroid/Firmware/build/bootloader/../include/sdkconfig.h \
+ /Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/esp32/include/rom/ets_sys.h \
+ /Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/soc/esp32/include/soc/soc.h \
+ /Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/log/include/esp_log_internal.h \
+ /Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/bootloader_support/include/bootloader_random.h
 
-/Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/bootloader_support/include/esp_efuse.h:
+/Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/bootloader_support/include/esp_efuse.h:
 
-/Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/soc/esp32/include/soc/efuse_reg.h:
+/Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/soc/esp32/include/soc/efuse_reg.h:
 
-/Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/soc/esp32/include/soc/soc.h:
+/Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/soc/esp32/include/soc/soc.h:
 
-/Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/esp32/include/esp_assert.h:
+/Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/esp32/include/esp_assert.h:
 
-/Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/esp32/include/esp_err.h:
+/Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/esp32/include/esp_err.h:
 
-/Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/log/include/esp_log.h:
+/Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/log/include/esp_log.h:
 
-/Users/eugene/Desktop/github/RetroESP32/Public/Firmware/Firmware/build/bootloader/../include/sdkconfig.h:
+/Users/andrueu/Desktop/github/R32/Odroid/Firmware/build/bootloader/../include/sdkconfig.h:
 
-/Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/esp32/include/rom/ets_sys.h:
+/Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/esp32/include/rom/ets_sys.h:
 
-/Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/soc/esp32/include/soc/soc.h:
+/Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/soc/esp32/include/soc/soc.h:
 
-/Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/log/include/esp_log_internal.h:
+/Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/log/include/esp_log_internal.h:
 
-/Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/bootloader_support/include/bootloader_random.h:
-/Users/eugene/Desktop/github/RetroESP32/Public/Software/Tools/esp-idf/components/bootloader_support/src/efuse.c:
+/Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/bootloader_support/include/bootloader_random.h:
+/Users/andrueu/Desktop/github/R32/Software/Tools/esp-idf/components/bootloader_support/src/efuse.c:
